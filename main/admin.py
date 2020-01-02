7190c591eaf5c94fb87cd88a01dbf61e74515306
Josh Arrants <duhhobo@gmail.com>
2020-01-02 02:03:18 -0500

    Modified models to add category and series to tutorials

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from django.db import models
 from ckeditor.widgets import CKEditorWidget
 
@@ -9,7 +9,9 @@ from ckeditor.widgets import CKEditorWidget
 class TutorialAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Title/date', {'fields': ['title', 'description', 'published']}),
-        ('Content', {'fields': ['content']}),
+        ('URL', {'fields': ['slug']}),
+        ('Series', {'fields': ['series']}),
+        ('Content', {'fields': ['content']})
     )
 
     formfield_overrides = {
@@ -17,4 +19,6 @@ class TutorialAdmin(admin.ModelAdmin):
     }
 
 
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial, TutorialAdmin)
